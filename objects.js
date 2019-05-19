891e6554c84cad7b8ffa7239189015188c48e359
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-05-19 17:08:21 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
   return playlist ;
 } 
-function removeFromPlaylist(playlist, ) {
+function removeFromPlaylist(playlist, Slowdive) {
   delete playlist.songTitle; 
   return playlist;
 }
